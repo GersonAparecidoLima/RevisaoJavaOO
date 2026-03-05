bffba3c4d1a34f374c0b5587e3ab461e9d3be6cd
GersonAparecidoLima <gersonaparecido@gmail.com>
2026-03-05 11:53:33 -0300

    classe

---
+++ RevisoaOreintacaoObjeto/src/br/com/alura/Funcionario.java
@@ -0,0 +1,34 @@
+package br.com.alura;
+
+public class Funcionario {
+	
+	String nome;
+	String cargo;
+	double salario;
+	
+	/*
+	 * public void exibirInformacoes() {
+    System.out.printf("Funcionario %s - Cargo: %s - Salário %.2f",
+            nome, cargo, salario);
+}
+	 * */
+
+	public void exibirInformacoes() {
+		System.out.printf("Funcionario %s - Cargo: %s - Salário %.2f \n", nome, cargo, salario);
+	}
+	
+	
+	/*
+	 * public void reajustarSalario(double percentual) {
+    salario += salario * (percentual / 100);
+    System.out.printf("\nNovo salario de %s é %.2f ", nome, salario);
+}
+	 * */
+
+	
+	public void reajustarSalario(double percentual) {
+		salario += salario * (percentual/100);
+		System.out.printf("\nNovo salario de %s é %.2f \n", nome, salario);
+		
+	}
+}
